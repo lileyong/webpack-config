50363c6f8658e33d1a72802bead522cfd5686105
promise.li <promise.li@corp.to8to.com>
2019-09-29 14:16:10 +0800

    splitChunks

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,57 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { VueLoaderPlugin } = require('vue-loader')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = function (env, argv) {
-    return new Promise((resolve, reject) => {
-        resolve({
-            mode: env.production ? 'production' : 'development',
-            devServer: {
-                open: true
-            },
-            module: {
-                rules: [{
-                    enforce: 'pre',
-                    test: /\.(js|vue)$/,
-                    loader: 'eslint-loader',
-                    options: {
-                        fix: true
-                    }
-                }, {
-                    test: /\.vue$/,
-                    use: 'vue-loader'
-                }]
-            },
-            plugins: [
-                new HtmlWebpackPlugin(),
-                new VueLoaderPlugin()
-            ],
-            resolve: {
-                alias: {
-                    src: path.resolve(__dirname, 'src')
+    const obj = {
+        mode: env.production ? 'production' : 'development',
+        entry: {
+            main: 'src/index.js',
+            vendor: ['vue']
+        },
+        devServer: {
+            open: true
+        },
+        module: {
+            rules: [{
+                enforce: 'pre',
+                test: /\.(js|vue)$/,
+                loader: 'eslint-loader',
+                options: {
+                    fix: true
                 }
-            },
-            output: {
+            }, {
+                test: /\.vue$/,
+                use: 'vue-loader'
+            }]
+        },
+        plugins: [
+            new HtmlWebpackPlugin(),
+            new VueLoaderPlugin()
+        ],
+        resolve: {
+            alias: {
+                src: path.resolve(__dirname, 'src')
             }
-        })
-    })
+        },
+        optimization: {
+            splitChunks: {
+                name: 'vendor',
+                chunks: 'all'
+            }
+        },
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: '[name].[chunkhash:8].js'
+        }
+    }
+
+    if (env.production) {
+        obj.plugins.push(
+            new CleanWebpackPlugin()
+        )
+    }
+
+    return obj
 }

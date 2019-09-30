66d125f34ff5352981f5b2e559a7afca184c80e7
promise.li <promise.li@corp.to8to.com>
2019-09-30 16:14:25 +0800

    add element-ui

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from 'src/App.vue'
 import router from 'src/router/index.js'
+import ElementUI from 'element-ui'
+
+Vue.use(ElementUI)
 
 const root = document.createElement('div')
 document.body.appendChild(root)

da05f5df5287cb4e7126a9e5fca323a7e19fcd66
promise.li <promise.li@corp.to8to.com>
2019-09-30 10:49:24 +0800

    添加vscode插件支持

--- src/App.vue
+++ src/App.vue
@@ -11,12 +11,14 @@ export default {
 </script>
 
 <style lang="scss">
-    html, body {
-        margin: 0;
-        padding: 0;
-        height: 100%;
-    }
-    #app {
-        height: 100%;
-    }
+html,
+body {
+    margin: 0;
+    padding: 0;
+    height: 100%;
+}
+
+#app {
+    height: 100%;
+}
 </style>

6c875b1b3578d6b8c7cce6800630c54ad72e34b5
李乐勇 <lileyong@liyueyongs-MBP.lan>
2019-10-21 00:36:26 +0800

    add vuex

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from 'src/App.vue'
 import router from 'src/router/index.js'
+import store from 'src/store/index.js'
 import ElementUI from 'element-ui'
 
 Vue.use(ElementUI)
@@ -10,5 +11,6 @@ document.body.appendChild(root)
 
 new Vue({
     router,
+    store,
     render: (h) => h(App)
 }).$mount(root)

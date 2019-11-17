3dfde5c678cd69f390ece761ba27221c893ab7e1
李乐勇 <lileyong@liyueyongs-MBP.lan>
2019-11-17 17:34:48 +0800

    add my-plugin

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import App from 'src/App.vue'
 import router from 'src/router/index.js'
 import store from 'src/store/index.js'
 import ElementUI from 'element-ui'
+import MyPlugin from '@promise.li/my-plugin'
 
 Vue.use(ElementUI)
+Vue.use(MyPlugin)
 
 const root = document.createElement('div')
 document.body.appendChild(root)

e25eae93ac5d7489ac9dc67a8ef048a7c9b296d0
李乐勇 <lileyong@liyueyongs-MacBook-Pro.local>
2020-01-24 00:10:24 +0800

    excel

--- src/views/export/index.vue
+++ src/views/export/index.vue
@@ -6,6 +6,8 @@
 </template>
 
 <script>
+import student from './student'
+
 export default {
     mounted () {
         const node = document.querySelector('.el-button--default')
@@ -28,37 +30,23 @@ export default {
     methods: {
         doExport () {
             const ExportJsonExcel = require('js-export-excel')
+            var sheetData = []
+            student.forEach(item => {
+                const name = item.match(/(?<=幼儿姓名：).+?\s/)
+                sheetData.push({
+                    s1: name, s2: item.match(/(?<=寒假居留城市：).+?\s/), s3: item.match(/(?<=外出居留城市：).+?\s/), s4: item.match(/(?<=有无居留武汉：).+?\s/), s5: item.match(/(?<=离开深圳时间：).+?\s/), s6: item.match(/(?<=返回深圳时间：).+?\s/), s7: item.match(/(?<=家属有无居留武汉：).+?\s/), s8: item.match(/(?<=有无发烧咳嗽等症状：).+?\s/), s9: '',
+                })
+            })
 
             var option = {}
             option.datas = [
                 {
-                    sheetData: [
-                        {
-                            one: '一行一列',
-                            two: '一行二列',
-                        },
-                        {
-                            one: '二行一列',
-                            two: '二行二列',
-                        },
-                    ],
+                    sheetData,
                     sheetName: 'sheet',
-                    sheetFilter: ['two', 'one'],
-                    sheetHeader: ['第一列', '第二列'],
+                    sheetFilter: ['s1', 's2', 's3', 's4', 's5', 's6', 's7', 's8', 's9'],
+                    sheetHeader: ['学生姓名', '寒假居留城市', '外出居留城市', '有无居留武汉', '离深时间', '返深时间', '家属有无居留武汉', '有无发烧', '咳嗽等症状', '备注'],
                     columnWidths: [20, ''],
-                },
-                {
-                    sheetData: [
-                        {
-                            one: '一行一列',
-                            two: '一行二列',
-                        },
-                        {
-                            one: '二行一列',
-                            two: '二行二列',
-                        },
-                    ],
-                },
+                }
             ]
             var toExcel = new ExportJsonExcel(option)
             toExcel.saveExcel()

cc7b4569eb479a73514d0a6bab9d6459b17729cf
promise.li <promise.li@corp.to8to.com>
2020-03-12 17:12:03 +0800

    add prettierrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,21 @@
 module.exports = {
     env: {
         browser: true,
-        es6: true
+        es6: true,
     },
-    extends: ["standard", "plugin:vue/essential"],
+    extends: ['standard', 'plugin:vue/essential'],
     globals: {
-        Atomics: "readonly",
-        SharedArrayBuffer: "readonly"
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
     },
     parserOptions: {
         ecmaVersion: 2018,
-        sourceType: "module"
+        sourceType: 'module',
     },
-    plugins: ["vue"],
+    plugins: ['vue'],
     rules: {
-        indent: ["error", 4],
-        "no-new": "off",
-        "comma-dangle": "off"
-    }
-};
+        indent: ['error', 4],
+        'no-new': 'off',
+        'comma-dangle': 'off',
+    },
+}
--- src/index.js
+++ src/index.js
@@ -12,5 +12,5 @@ document.body.appendChild(root)
 new Vue({
     router,
     store,
-    render: (h) => h(App)
+    render: h => h(App)
 }).$mount(root)

2bc4c9486f6dda0102f9e6dbc5c8eb290e044cf3
promise.li <promise.li@corp.to8to.com>
2020-05-17 00:55:06 +0800

    webpack bundle analyze

--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -2,18 +2,23 @@ const merge = require('webpack-merge')
 const baseConfig = require('./webpack.base')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+    .BundleAnalyzerPlugin
 
-module.exports = merge(baseConfig, {
-    mode: 'production',
-    plugins: [
-        new CleanWebpackPlugin(),
-        new MiniCssExtractPlugin({
-            filename: '[name].[chunkhash:8].css'
-        })
-    ],
-    optimization: {
-        splitChunks: {
-            chunks: 'all'
+module.exports = function (env = {}) {
+    return merge(baseConfig, {
+        mode: 'production',
+        plugins: [
+            new CleanWebpackPlugin(),
+            new MiniCssExtractPlugin({
+                filename: '[name].[chunkhash:8].css'
+            }),
+            ...(env.analyze ? [new BundleAnalyzerPlugin()] : [])
+        ],
+        optimization: {
+            splitChunks: {
+                chunks: 'all'
+            }
         }
-    }
-})
+    })
+}
